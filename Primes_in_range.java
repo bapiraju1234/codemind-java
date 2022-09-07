449347cd82430505951e216bbe6a9cca8dca778c
bapiraju1234 <112751566+bapiraju1234@users.noreply.github.com>
2022-09-07 21:02:18 +0530

    import java.util.Scanner; class simple {     public static boolean prime(int n)     {         int cont=0;
            for(int i=2;i<=(int)(Math.sqrt(n));i++)
            {
                if(n%i==0)
                cont++;
            }
            if(cont==0)
                return true;
            else
                return false;
        }
        public static void main(String args[])
        {
            Scanner sc=new Scanner(System.in);
            int a,b,cont=0;
            a = sc.nextInt();
            b = sc.nextInt();
            if(a<2)
            a = 2;
            for(int j=a;j<=b;j++)
            {
                if(prime(j))
                cont++;
            }
            System.out.println(cont);
        }
    }

---
+++ Primes_in_range.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class simple
+{
+    public static boolean prime(int n)
+    {
+        int cont=0;
+
+        for(int i=2;i<=(int)(Math.sqrt(n));i++)
+        {
+            if(n%i==0)
+            cont++;
+        }
+        if(cont==0)
+            return true;
+        else
+            return false;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b,cont=0;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        if(a<2)
+        a = 2;
+        for(int j=a;j<=b;j++)
+        {
+            if(prime(j))
+            cont++;
+        }
+        System.out.println(cont);
+    }
+}
