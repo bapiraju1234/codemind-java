71df96e8ee57dd6b11b8aeaec29807dbcb8a68f0
bapiraju1234 <112751566+bapiraju1234@users.noreply.github.com>
2022-09-28 18:41:12 +0530

    import java.util.Scanner; class simple {      public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],i,cont=0,k;         n = sc.nextInt();         x = new int[n];         for(i=0;i<n;i++)             x[i] = sc.nextInt();         k = sc.nextInt();         for(i=0;i<n;i++)         {             if(x[i]==k)                 cont++;         }         if(cont>=1)         System.out.println("True");         else         System.out.println("False");     } }

---
+++ element_search_in_an_array.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class simple
+{
+     public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],i,cont=0,k;
+        n = sc.nextInt();
+        x = new int[n];
+        for(i=0;i<n;i++)
+            x[i] = sc.nextInt();
+        k = sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            if(x[i]==k)
+                cont++;
+        }
+        if(cont>=1)
+        System.out.println("True");
+        else
+        System.out.println("False");
+    }
+}
