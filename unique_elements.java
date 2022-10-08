9eafba013b3d7c7092c4b82962f85a13db707c36
bapiraju1234 <112751566+bapiraju1234@users.noreply.github.com>
2022-10-08 14:52:09 +0530

    import java.util.*; class Sample {     public static void main(String args[]) {     Scanner sc=new Scanner(System.in);     int n,x[],i,j,k,count=0,sum=0;     n=sc.nextInt();     x=new int[n];     for(i=0;i<n;i++)         x[i]=sc.nextInt();         for(i=0;i<n;i++)         {             if(x[i]!=-999)             {                 //count=1;                 for(j=0;j<n;j++)                 {                     if(x[i]==x[j] && i!=j)                     {                         // count++;                         x[j]=-999;                     }
                    }
                    System.out.print(x[i]+" ");
                }
            }
                    
                    
    }
    }

---
+++ unique_elements.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Sample
+{
+    public static void main(String args[])
+{
+    Scanner sc=new Scanner(System.in);
+    int n,x[],i,j,k,count=0,sum=0;
+    n=sc.nextInt();
+    x=new int[n];
+    for(i=0;i<n;i++)
+        x[i]=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            if(x[i]!=-999)
+            {
+                //count=1;
+                for(j=0;j<n;j++)
+                {
+                    if(x[i]==x[j] && i!=j)
+                    {
+                        // count++;
+                        x[j]=-999;
+                    }
+                    
+                }
+                System.out.print(x[i]+" ");
+            }
+        }
+                
+                
+}
+}
+
